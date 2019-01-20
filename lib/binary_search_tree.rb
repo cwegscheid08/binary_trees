4ae8c260dbef10027cd730de9aba98c2cde48bb8
Chris Wegscheid <cwegscheid08@outlook.com>
2019-01-20 18:00:40 -0500

    Reworking node to compose tree competely

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,90 +4,10 @@ class BinarySearchTree
 	require './lib/node.rb'
 
 	def initialize
-		@root = nil
-	end
-
-	def crawl_up
-		@tmp = @tmp.parent
-	end
-
-	def crawl_left
-		@tmp = @tmp.child_left
-	end
-
-	def crawl_right
-		@tmp = @tmp.child_right
-	end
-
-	def build_tree(array)
-		array.sort!
-
-		puts "ARRAY: #{array}"
-
-		if array.size <= 1
-
-			@tmp = Node.new(array[0], nil, nil,nil)
-			return @tmp
-
-		elsif array.size == 2
-			puts "ARRAY: #{array}  SIZE: #{array.size}"
-			if array[0] < array[1]
-				child = Node.new(array[0], nil, nil, nil)
-				@tmp = Node.new(array[1], child, nil, nil)
-				child.parent = @tmp
-			elsif array[0] > array[1]
-				child = Node.new(array[1], nil, nil, nil)
-				@tmp = Node.new(array[0], child, nil, nil)
-				child.parent = @tmp
-			end
-				
-			return @tmp
 		
-			
-			# child_left = array[0]
-			# child_right = array[2]
-		end
-
-		a = array[(array.size/2)]
-		b = build_tree(array.slice!(((array.size/2)+1), array.size-1))
-		c = build_tree(array)
-
-		puts "TMP:#{@tmp.value}\nA:#{a}\nB:#{b.value}\nC:#{c.value}\n"
-
-		@root == nil ? @root = @tmp : ""
-
-		if b.value < c.value && c.child_left == nil
-			c.child_left = b
-			b.parent = c
-		elsif b.value > c.value && c.child_right == nil
-			c.child_right = b
-			b.parent = c
-		end
-			
-		# puts "B PARENT: #{b.parent.value} C PARENT: #{c.parent.value}"
-
-		# if b.value < @tmp.value && @tmp.child_left == nil
-		# 	@tmp.child_left = b
-		# 	b.parent = @tmp
-		# elsif b.value > @tmp.value && @tmp.child_right == nil
-		# 	@tmp.child_right = b
-		# 	b.parent = @tmp
-		# elsif b.value < @tmp.value && @tmp.child_right != nil
-		# 	b.child_right = @tmp
-		# 	@tmp.parent = b
-		# elsif b.value > @tmp.value && @tmp.child_left != nil
-		# 	b.child_left = @tmp
-		# 	@tmp.parent = b
-		# end
-		c
 	end
 
-	def write_out
-		until @tmp.parent == nil 
-			puts " VALUE: #{@tmp.value}"
-			crawl_up
-		end
-	end
+	
 
 end
 
--- lib/node.rb
+++ lib/node.rb
@@ -1,33 +1,101 @@
-class Node < BinarySearchTree
-	attr_accessor :key, :child_left, :child_right, :parent
+class Node
+	attr_accessor :value, :child_left, :child_right, :root, :crawler_node
 
-
-	def initialize(value, child_left, child_right, parent)
-		@key = value
-		@child_left = child_left
-		@child_right = child_right
-		@parent = parent
+	def initialize(value = nil)
+		@value = value
+		@root = nil
+		@child_left = nil
+		@child_right = nil
 	end
 
-	def children
-		puts "LEFT: #{child_left.value}  RIGHT: #{child_right.value}"
+	def rooter
+		@crawler_node = @root
 	end
 
-	def value
-		@key
-	end
+	def build_tree(array)
+		return array[0] if array.size == 1
 
-	def setter
+		mid = (array.length/2)
+		@root.nil? ? (@root = Node.new(array[mid]); @crawler_node = @root): ""
+
+		right = build_tree(array.slice!(mid, array.size-1))
+		left = build_tree(array)
+
+		left.class == Integer ? left = Node.new(left) : ""
+		right.class == Integer ? right = Node.new(right) : ""
+
+		# puts "CRAWLER:#{crawler_node.value} LEFT:#{left.value} RIGHT:#{right.value}"
+
+		# print "CRAWLER:#{@crawler_node.value} "
+		# print left.value.nil? ? "" : "LEFT:#{left.value} "
+		# print right.value.nil? ? "" : "RIGHT:#{right.value} "
+
+		rooter
+		place_node(left)
+		rooter
+		place_node(right)
+
+
 		
-	end
-
-	def getter
 
 	end
 
+	def place_node(node)
 
+		print "NODE:#{node} "
+		print "CLASS:#{node.class} "
+		print node.value.nil? ? "" : "VALUE:#{node.value} "
+		print node.child_left.nil? ? "" : "LEFT:#{node.child_left.value} "
+		print node.child_right.nil? ? "" : "RIGHT:#{node.child_right.value} "
+		puts
+
+
+
+		if node.value < @crawler_node.value && @crawler_node.child_left.nil?
+			@crawler_node.child_left = node
+		elsif node.value > @crawler_node.value && @crawler_node.child_right.nil?
+			@crawler_node.child_right = node
+		elsif node.value < @crawler_node.value && node.value > @crawler_node.child_left.value
+			node.child_left = @crawler_node.child_left
+			@crawler_node.child_left = node
+		elsif node.value > @crawler_node.value && node.value < @crawler_node.child_right.value
+			node.child_right = @crawler_node.child_right
+			@crawler_node.child_right = node
+		elsif node.value < @crawler_node.value && node.value < @crawler_node.child_left.value
+			@crawler_node = @crawler_node.child_left
+			place_node(node)
+		elsif node.value > @crawler_node.value && node.value > @crawler_node.child_right.value
+			@crawler_node = @crawler_node.child_right
+			place_node(node)
+		else
+			puts "\nERROR\n"
+		end
+			
+
+
+		# if @crawler_node.child_left.nil? || @crawler_node.child_left.value < node.value
+		# 	# WRITE CODE FOR CREATING NODE OR SHIFTING NODE
+		# 	node.child_left = @crawler.child_left
+		# 	@crawler.child_left = node
+		# elsif @crawler_node.child_right.nil? || @crawler_node.child_right.value > node.value
+		# 	node.child_right = @crawler.child_right
+		# 	@craler.child_right = node
+		# elsif node.value < @crawler_node.value && node.value < @crawler_node.child_left.value
+		# 	# SHIFT VALUE OF CRAWLER_NODE AND RECURSE
+		# 	@crawler = @crawler.child_left
+		# 	place_node(node)
+		# elsif node.value > @crawler_node.value && node.value > @crawler_node.child_right.value
+		# 	@crawler = @crawler.child_right
+		# 	place_node(node)
+		# end
+	end
 
 end
 
 
-
+node = Node.new
+array = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
+array.sort!
+# node.build_tree([1,7,4,23,8,9,4,3,5,7,9,67,6345,324])
+puts "ARRAY: #{array.to_s}"
+node.build_tree(array)
